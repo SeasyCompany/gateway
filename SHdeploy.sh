090ac3d1f4208cf2c38c8059b89ffdcb125291df
Vinicius Motta <viniciusmotta8@gmail.com>
2021-10-16 16:44:11 -0300

    script updated

--- SHdeploy.sh
+++ SHdeploy.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 
 git checkout development
+git fetch
+git pull origin development
+yarn build
 yq eval -i '.provider.stage = "dev"' serverless.yml
 sls deploy -v
 git checkout .
 
 cd ../shopee
 git checkout development
+git fetch
+git pull origin development
+yarn build
 yq eval -i '.provider.stage = "dev"' serverless.yml
 sls deploy -v
 git checkout .
 
 cd ../meli
 git checkout development
+git fetch
+git pull origin development
+yarn build
 yq eval -i '.provider.stage = "dev"' serverless.yml
 sls deploy -v
 git checkout .
--- SHupdate-package.sh
+++ SHupdate-package.sh
@@ -1,6 +1,8 @@
 #!/bin.sh
 
 git checkout development
+git fetch
+git pull origin development
 yarn add @vmotta8/price-comparison
 git add .
 git commit -m "price comparisson package updated"
@@ -8,6 +10,8 @@ git push origin development
 
 cd ../shopee
 git checkout development
+git fetch
+git pull origin development
 yarn add @vmotta8/price-comparison
 git add .
 git commit -m "price comparisson package updated"
@@ -15,6 +19,8 @@ git push origin development
 
 cd ../meli
 git checkout development
+git fetch
+git pull origin development
 yarn add @vmotta8/price-comparison
 git add .
 git commit -m "price comparisson package updated"
